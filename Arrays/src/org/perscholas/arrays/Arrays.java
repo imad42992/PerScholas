c34729a6b5e1de6c1f536e5a955a204c53793f5e
imad42992 <imad42992@gmail.com>
2020-03-02 11:29:21 -0600

    changes made to practice arrays

--- Arrays/src/org/perscholas/arrays/Arrays.java
+++ Arrays/src/org/perscholas/arrays/Arrays.java
@@ -4,9 +4,14 @@ public class Arrays {
 
 	public static void main(String[] args) {
 		
-		int[] jd = {1,2,3,4,5};
-		System.out.println(jd[1]);
-		
-	}
-
+		int result = 0;
+		for (int i = 0; i < 5; i++) {
+		  if (i == 3) { 
+		     result += 10;
+		   } else {
+		     result += i;
+		   }	
+		}
+		System.out.println(result);
+}
 }
